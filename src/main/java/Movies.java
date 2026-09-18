ee3ee5d23f48d8e0a4f8d63f5233ad66e487917f
Stefan Bergli <ascendens@hotmail.com>
2026-09-18 07:27:03 +0200

    Färdig med förklaringar

--- src/main/java/Movies.java
+++ src/main/java/Movies.java
@@ -2,7 +2,10 @@ public class Movies {
     static void main(){
 
         int gräns = 120;
-
+        // film1 som skapas här under är som en vägskylt som pekar på en plats i minnet där ett Film-objekt finns.
+        // I parentesen specificeras vad som finns i Film-objektet på den platsen i minnet.
+        // För att hämta saker från Film-objektet anger man namnet på vägskylten och vilken sak man vill ha.
+        // Vill man ha titeln på filmen i film2 skriver man film2.title().
         Film film1 = new Film("Alien","Ridley Scott", 117);
         Film film2 = new Film ("Jaws", "Steven Spielberg", 124);
 
@@ -17,7 +20,7 @@ public class Movies {
         return  f.title + " är längre än gränsen.";
 
     }
-
+    //record bestämmer vad som kan finnas i en behållare av typen Film.
     record Film(String title, String director, int time){
 
     }
